21fde6003e99bac9a54feee80a4465b23237b11f
R3D <cristophermendezcervantes@gmail.com>
2026-05-23 11:13:48 +0100

    added switch behavior

---
+++ is_checked.js
@@ -0,0 +1,27 @@
+/*
+
+        ===== DEV MAINTENANCE ===== 
+
+        1º this file is meant to be used as a switch.
+        2º by default, no parameters are accepted during fetch handling.
+        3º if the checkbox is unchecked, a combobox is displayed for the user.
+        4º if the checkbox is checked, a combobox is checked for GET/POST/PUT/DELETE params.
+
+*/
+
+
+function checkListener(){
+    const isCheckedParameters = document.getElementById('checkParam').checked;
+    const combobox = document.querySelector('.check');
+
+    if (isCheckedParameters){
+        combobox.style.display = "none";
+        combobox.selectedIndex = 0;
+    }
+    else{
+        combobox.style.display = "block";
+    }
+}
+
+
+
