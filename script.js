bc71b9fee3162654e6c687d078c0f47ea288e4e2
R3D <cristophermendezcervantes@gmail.com>
2026-05-23 13:53:20 +0100

    refactor and body building

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+import {displayStatus} from "./http_options.js"
+
 //api fetch logic
 let jsonData = null;
 async function btnFetch(){
@@ -8,6 +10,8 @@ async function btnFetch(){
     const cardBody = document.querySelector('.apicard');
     const background = document.querySelector('.glass-success');
     const btnDownloadJSON = document.querySelector('.jsonbtn');
+    const currentStatus = displayStatus();
+    console.log(currentStatus);
 
     if (apiUri.value.trim() === "") {
         return;
@@ -16,49 +20,70 @@ async function btnFetch(){
     let totalTimeIntervall = null;
     //delaying ms are mandatory
     const startingTimeInterval = performance.now()
-    try {
-        const response = await fetch(apiUri.value);
-        const timeOut = performance.now()
-        codeStatus = response.status;
-        totalTimeIntervall = (timeOut - startingTimeInterval).toFixed(2);
-        let timeTag = document.getElementById('time')
-        timeTag.textContent = totalTimeIntervall;
-        //provides the status of the response
-        status.textContent = response.status;
-        if (!response.ok) {
-            throw new Error(`Response: ${response.status}`);
-        }
+    const bodyContent = document.getElementById('body');
 
-        const contentType = response.headers.get("content-type");
+    // controlling the status we can have a different fetch option
+    switch (currentStatus) {
+        case 'GET':
+            try {
+                const response = await fetch(apiUri.value);
+                const timeOut = performance.now();
+                codeStatus = response.status;
+                totalTimeIntervall = (timeOut - startingTimeInterval).toFixed(2);
+                let timeTag = document.getElementById('time');
+                timeTag.textContent = totalTimeIntervall;
+                status.textContent = response.status;
 
-        //blob fallback
-        if (contentType && contentType.includes("image/")) {
-            console.log("es imagen")
-            const imageBlob = await response.blob(); 
-            const imageUrl = URL.createObjectURL(imageBlob);
-            apiContent.innerHTML = `<img src="${imageUrl}" class="img-fluid rounded" alt="Fetched Image">`;
-            btnDownloadJSON.style.display = "none"; 
-            cardBody.style.display = "block"; 
-            background.style.backgroundColor = "";
-        }
-        else{
-            const body = await response.json();
-            background.style.backgroundColor = ""; 
-            const currentJSONData = JSON.stringify(body, null, 2)
-            jsonData = currentJSONData;
-            apiContent.innerHTML = "<pre>" + currentJSONData + "</pre>";
-            cardBody.style.display = "block";
-            btnDownloadJSON.style.display = "block";
-        }
+                if (!response.ok) {
+                    throw new Error(`Response: ${response.status}`);
+                }
 
-    } catch (error) {
-        cardBody.style.display = "block";
-        background.style.setProperty('background-color', '#f58690', 'important');
-        status.textContent = codeStatus;
-        apiContent.textContent = error.message;
-        btnDownloadJSON.style.display = "none";
+                const contentType = response.headers.get("content-type");
+
+                if (contentType && contentType.includes("image/")) {
+                    const imageBlob = await response.blob();
+                    const imageUrl = URL.createObjectURL(imageBlob);
+                    apiContent.innerHTML = `<img src="${imageUrl}" class="img-fluid rounded" alt="Fetched Image">`;
+                    btnDownloadJSON.style.display = "none";
+                    cardBody.style.display = "block";
+                    background.style.backgroundColor = "";
+
+                } else {
+
+                    const body = await response.json();
+                    background.style.backgroundColor = "";
+                    const currentJSONData = JSON.stringify(body, null, 2);
+                    jsonData = currentJSONData;
+                    apiContent.innerHTML = "<pre>" + currentJSONData + "</pre>";
+                    cardBody.style.display = "block";
+                    btnDownloadJSON.style.display = "block";
+                }
+
+            } catch (error) {
+
+                cardBody.style.display = "block";
+                background.style.setProperty('background-color', '#f58690', 'important');
+                status.textContent = codeStatus;
+                apiContent.textContent = error.message;
+                btnDownloadJSON.style.display = "none";
+            }
+
+            break;
+        case 'POST':
+            window.alert("There is nothing to do with POST statement");
+            break;
+        case 'PUT':
+            window.alert("There is nothing to do with PUT statement");
+            break;
+        case 'DELETE':
+            window.alert("There is nothing to do with DELETE statement");
+            break;
+        default:
+            break;
     }
 }
+
+
 function downloadJSONFile(){
     if (jsonData){
         const blob = new Blob([jsonData], {type:'application/json'});
@@ -81,3 +106,8 @@ function reset(){
     document.getElementById("api-content").textContent = "";
     document.getElementById("api-result").textContent = "";
 }
+
+window.btnFetch = btnFetch;
+window.reset = reset;
+window.displayStatus = displayStatus;
+window.downloadJSONFile = downloadJSONFile;
